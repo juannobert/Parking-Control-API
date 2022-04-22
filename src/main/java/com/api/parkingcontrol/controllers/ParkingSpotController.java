acea48f6e8c8d4e7f114acb0f72c3bdf4c0e98e8
Juan Nobert <nobert407@gmail.com>
2022-04-22 09:27:41 -0300

    Controller criado

---
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -0,0 +1,16 @@
+package com.api.parkingcontrol.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.parkingcontrol.services.ParkingSpotService;
+
+@RestController
+@CrossOrigin(origins = "*",maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+	@Autowired
+	ParkingSpotService service;
+}
