80076856e44969936fd9d651eab8e929b9f3fee6
Juan Nobert <nobert407@gmail.com>
2022-04-22 09:23:04 -0300

    Service e Repository criados

---
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,5 @@
+package com.api.parkingcontrol.repositories;
+
+public class ParkingSpotRepository {
+
+}
---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,5 @@
+package com.api.parkingcontrol.services;
+
+public class ParkingSpotService {
+
+}
